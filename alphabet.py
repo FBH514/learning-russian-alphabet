26835ca5241c4b32e33508d402c2b836fcbdc86b
FBH514 <103460138+FBH514@users.noreply.github.com>
2023-02-02 15:41:06 -0500

    Refactored code

--- alphabet.py
+++ alphabet.py
@@ -5,42 +5,85 @@ from typing import Optional
 class Alphabet:
 
     def __init__(self) -> None:
+        """
+        Initializes the Alphabet object
+        """
         self.letters: list = []
 
     def cyrillic_alphabet(self) -> None:
+        """
+        Cyrillic alphabet
+        :return: list of cyrllic letters
+        """
         self.letters = ['А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я']
 
     def latin_alphabet(self) -> None:
+        """
+        Latin alphabet
+        :return: list of latin letters
+        """
         self.letters = ['A', 'B', 'V', 'G', 'D', 'E', 'Jo', 'Zh', 'Z', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'R', 'S', 'T', 'U', 'F', 'Kh', 'C', 'Ch', 'Sh', 'Shh', '\"', 'Y', '\'', 'Eh', 'Ju', 'Ja']
 
     def random_index(self) -> Optional[int]:
+        """
+        Returns random index of the letter
+        :return: random index
+        """
         if len(self.letters) > 0:
             return random.randint(0, len(self.letters) - 1)
         return None
 
     def random_letter(self, index) -> Optional[str]:
+        """
+        Returns random letter
+        :param index: index of the letter
+        :return:
+        """
         if index is not None:
             return self.letters[index]
         return None
 
     def remove_letter(self, index) -> None:
+        """
+        Removes letter from the list
+        :param index: index of the letter to remove
+        :return: None
+        """
         if index is not None:
             self.letters.pop(index)
 
 class CyrillicAlphabet(Alphabet):
 
     def __init__(self) -> None:
+        """
+        Initializes the CyrillicAlphabet object
+        Inherits from Alphabet
+        :return: None
+        """
         super().__init__()
         self.cyrillic_alphabet()
 
     def __repr__(self) -> str:
+        """
+        Returns string representation of the object
+        :return: string representation of the object
+        """
         return f"Cyrillic Alphabet {' '.join(self.letters)}"
 
 class LatinAlphabet(Alphabet):
 
     def __init__(self) -> None:
+        """
+        Initializes the LatinAlphabet object
+        Inherits from Alphabet
+        :return: None
+        """
         super().__init__()
         self.latin_alphabet()
 
     def __repr__(self) -> str:
+        """
+        Returns string representation of the object
+        :return: string representation of the object
+        """
         return f"Latin Alphabet {' '.join(self.letters)}"
--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import re
-
 from alphabet import CyrillicAlphabet, LatinAlphabet
 
 cyrillic = CyrillicAlphabet()
 latin = LatinAlphabet()
 
+score = {"marks": 0, "total": 0}
+
 def main() -> None:
-    print("Welcome to the quiz 'Guess the Cyrillic letter'!")
+    """
+    Main function
+    :return: None
+    """
+    print("Welcome to 'Guess the Cyrillic letter'!")
     while True:
         random_index = cyrillic.random_index()
         random_letter = cyrillic.random_letter(random_index)
@@ -17,21 +22,47 @@ def main() -> None:
                 main()
             else:
                 break
+    print(f'Your score is {score["marks"]} out of {score["total"]}')
+
+
+def keep_score(mark: int = 1, total: int = 1) -> None:
+    """
+    Keeps score of the game
+    :param mark: mark to add to the score
+    :param total: total to add to the score
+    :return: None
+    """
+    score["marks"] += mark
+    score["total"] += total
 
 
 def user_input(random_letter: str, random_index: int) -> None:
+    """
+    Asks user to guess the letter
+    :param random_letter: random letter
+    :param random_index: random index
+    :return: None
+    """
     if random_letter is None:
         return
     while True:
         user_input = input(f'Enter the letter {random_letter}: ').lower()
-        if latin.letters[random_index].lower() == user_input:
-            print('Correct!')
+        if user_input.isspace() or user_input == '':
+            continue
+        elif latin.letters[random_index].lower() == user_input.lower():
+            keep_score()
+            print('Correct!\n')
         else:
-            print('Incorrect!')
-        print()
+            keep_score(0)
+            print(f'Incorrect! Answer was {latin.letters[random_index]}\n')
         break
 
-def again():
+
+def again() -> bool:
+    """
+    Asks user if they want to play again
+    :return: True if user wants to play again, False otherwise
+    """
     while True:
         user_input = input('Play again? (y/n): ').lower()
         if re.match(r'^(y|yes|ya|yeah)$', user_input):
@@ -41,7 +72,13 @@ def again():
         else:
             print('Incorrect input!')
 
-def remove_letter(index) -> None:
+
+def remove_letter(index: int) -> None:
+    """
+    Removes letter from the list
+    :param index: index of the letter to remove
+    :return: None
+    """
     cyrillic.remove_letter(index)
     latin.remove_letter(index)
 
